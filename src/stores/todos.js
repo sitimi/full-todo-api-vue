29eb7aa00c50db3e912479b3f0edabaac1c97234
sitimi <sitimi522@gmail.com>
2024-06-26 10:22:53 +0900

    ストアから非同期処理を削除

--- src/stores/todos.js
+++ src/stores/todos.js
@@ -5,10 +5,6 @@ import { defineStore } from 'pinia'
 
 export const useTodosStore = defineStore('todos', () => {
   const todos = ref([{}])
-
-  function fetchTodosFromDb() {
-    // 非同期処理
-  }
-
-  return { todos, fetchTodosFromDb }
+  
+  return { todos }
 })
