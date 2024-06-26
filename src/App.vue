653fa679e88ae329baa4c7239bf6221f251352b2
sitimi <sitimi522@gmail.com>
2024-06-26 10:26:39 +0900

    マウントしたときにDBからフェッチするよう実装

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <script setup>
+import { onMounted } from 'vue';
+import { useTodosStore } from '@/stores/todos'
 import axios from 'axios';
 
+const todoStore = useTodosStore()
 const URL = "http://localhost:8080/todos";
-const getData = async ()=>{
-  const response = await axios.get(URL)
-  console.log(response.data)
+const fetchTodosFromDb = async () => {
+    const response = await axios.get(URL)
+    todoStore.todos = response.data
+  }
 
-}
+onMounted(async () => {
+  await fetchTodosFromDb()
+  console.log('---DBからフェッチしました---')
+})
 </script>
 
 <template>
   <v-app>
     <v-main>
-      <v-btn @click="getData">取得</v-btn>
+      
     </v-main>
   </v-app>
 </template>
