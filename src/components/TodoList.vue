8a81ed3a30603b28e17f315f3afe2123ccbe4210
sitimi <sitimi522@gmail.com>
2024-06-26 11:04:33 +0900

    Todo一覧表示機能実装

---
+++ src/components/TodoList.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { useTodosStore } from '@/stores/todos'
+
+const todoStore = useTodosStore()
+</script>
+<template>
+  <v-table>
+    <thead>
+      <tr>
+        <th class="text-left">
+          title
+        </th>
+        <th class="text-left">
+          text
+        </th>
+        <th class="text-left">
+          status
+        </th>
+        <th class="text-left">
+          due_date
+        </th>
+        <th class="text-left">
+          created_at
+        </th>
+        <th class="text-left">
+          updated_at
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="todo in todoStore.todos"
+        :key="todo.id"
+      >
+        <td>{{ todo.title }}</td>
+        <td>{{ todo.text }}</td>
+        <td>{{ todo.status }}</td>
+        <td>{{ todo.dueDate }}</td>
+        <td>{{ todo.createdAt }}</td>
+        <td>{{ todo.updatedAt }}</td>
+      </tr>
+    </tbody>
+  </v-table>
+</template>
