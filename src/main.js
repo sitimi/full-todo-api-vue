f7d2c97075a05007b45c891c21a6d52c6f93286d
sitimi <sitimi522@gmail.com>
2024-06-26 10:25:06 +0900

    pinia設定

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createApp } from 'vue'
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import { createPinia } from 'pinia'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import '@mdi/font/css/materialdesignicons.css'
@@ -17,5 +18,8 @@ const vuetify = createVuetify({
     iconfont: 'mdi' // 追加
   }
 })
-
-createApp(App).use(vuetify).mount('#app')
+const pinia = createPinia()
+const app = createApp(App)
+app.use(vuetify)
+app.use(pinia)
+app.mount('#app')
